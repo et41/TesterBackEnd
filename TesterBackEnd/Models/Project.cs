2226e6b118c9e683dd3eb7bd2e1020bb070a3ece
et41 <erenturan41@gmail.com>
2025-01-19 20:25:08 +0300

    Add project files.

---
+++ TesterBackEnd/Models/Project.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace TesterBackEnd.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string? ProjectId { get; set; }
+        public string? HvVoltage { get; set; }
+        public string? LvVoltage { get; set; }
+        public string? Taps { get; set; }
+        public string? RatedTap { get; set; }
+        public string? CustomerName { get; set; }
+        public string? VectorGroup { get; set; }
+        public string? Power { get; set; }
+
+        public string? DiffBetweenTaps { get; set; }
+
+        public string? LVTurns { get; set; }
+        public string? HVTurns { get; set; }
+        public string? TapTurns { get; set; }
+
+        public string? GuaranteedNoLoadLoss { get; set; }
+        public string? GuaranteedLoadLoss { get; set; }
+        public string? GuaranteedShortCircuitVoltage { get; set; }
+
+
+
+    }
+}
---
+++ TesterBackEnd/Models/TesterDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TesterBackEnd.Models
+{
+    public partial class TesterDBContext : DbContext
+    {
+
+        public TesterDBContext(DbContextOptions<TesterDBContext> options) : base(options) { }
+
+        public virtual DbSet<Project> Projects { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>(entity =>
+            {
+                entity.HasKey(k => k.Id);
+
+            });
+            OnModelCreating(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
